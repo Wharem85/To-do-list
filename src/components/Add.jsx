c330c58f269d51f65f4329afb1b9b6d9b7626279
Wharem85-25 <wharem85@hotmail.com>
2022-01-13 17:16:21 -0600

    Se agrego el input de la actividad y emojis

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import EmojiPicker from 'emoji-picker-react';
+import Picker from 'emoji-picker-react';
+import InputEmoji from "react-input-emoji";
 import '../assets/styles/Add.css';
 
 const Add = () => {
-	return (
-		<div>
+	const [text, setText] = useState("");
 
+  function handleOnEnter(text) {
+    console.log("enter", text);
+  }
+
+	return (
+		<div className="content">
+			<Box
+      	sx={{
+        	width: 600,
+        	maxWidth: '100%',
+      	}}
+    	>
+				<InputEmoji
+					className="activity"
+					id="inputActivity"
+      		value={text}
+      		onChange={setText}
+      		cleanOnEnter
+      		onEnter={handleOnEnter}
+      		placeholder="Escribe tu actividad"
+    		/>
+    	</Box>
 		</div>
 	);
 };
